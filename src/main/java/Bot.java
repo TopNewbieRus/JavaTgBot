14092620e4a87757680abf5e6d340d824f400a9b
TopNewbieRus <temnyy.egor@gmail.com>
2020-11-06 14:45:26 +0500

    Bot can write info about himself

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,7 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -17,6 +18,28 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
+    public void sendMsg(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public final Message sendMessage(SendMessage sendMessage) throws TelegramApiException{
+        if (sendMessage == null){
+            throw new TelegramApiException("Parameter sendMessege can not be null");
+        } else {
+            return (Message) this.sendApiMethod(sendMessage);
+        }
+    }
+
     @Override
     public String getBotUsername() {
         return "UrfuOOP_bot";
@@ -30,5 +53,12 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
+        if (message != null && message.hasText()){
+            switch (message.getText()){
+                case "/help":
+                    sendMsg(message, Bot_Info.Info());
+                break;
+            }
+        }
     }
 }
